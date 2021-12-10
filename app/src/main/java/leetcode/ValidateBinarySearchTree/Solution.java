2e9e12de7d40e5c291d9c2a226acaab4e60bb543
mkabumattar <mohammad.khaled@outlook.com>
2021-12-10 21:12:44 +0200

    98. Validate Binary Search Tree

---
+++ app/src/main/java/leetcode/ValidateBinarySearchTree/Solution.java
@@ -0,0 +1,22 @@
+package leetcode.ValidateBinarySearchTree;
+
+import leetcode.BinaryTreeNode.TreeNode;
+
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return traverse(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean traverse(TreeNode node, long low, long high) {
+
+        if(node == null){
+            return true;
+        }
+
+        if(node.val <= low || node.val >= high){
+            return false;
+        }
+
+        return traverse(node.left, low, node.val) && traverse(node.right, node.val, high);
+    }
+}
