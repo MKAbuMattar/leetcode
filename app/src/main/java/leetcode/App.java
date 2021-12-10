2c93353653f8d261f2e9c2457524125c278e494a
mkabumattar <mohammad.khaled@outlook.com>
2021-12-10 20:15:35 +0200

    14. Longest Common Prefix

--- app/src/main/java/leetcode/App.java
+++ app/src/main/java/leetcode/App.java
@@ -3,12 +3,9 @@
  */
 package leetcode;
 
-import leetcode.RomantoInteger.Solution;
-
 public class App {
 
     public static void main(String[] args) {
-        Solution sol = new Solution();
-        System.out.println(sol.romanToInt("III"));
+
     }
 }
